de7fd4761b848e82f822bf84c48def33050f84b8
pyos <pyos100500@gmail.com>
2016-03-22 18:51:51 +0300

    Make an attempt to not leak memory while transcoding.
    Actually, no, the transcoder does not work. Much safer
    to run the pipeline in a separate process anyway...

--- recoder.go
+++ recoder.go
@@ -3,41 +3,40 @@ package main
 import (
 	"errors"
 	"fmt"
+	// NOTE: pull changes from https://github.com/pyos/{glib,gst}
 	"github.com/ziutek/glib"
 	"github.com/ziutek/gst"
 	"strings"
 )
 
-func RunGlibForever() {
-	go glib.NewMainLoop(nil).Run()
-}
-
 type Demuxer struct {
 	//         |                      /-- pad video_%u -->
 	// stream -|-> source --> demux --+
 	//         |                      \-- pad audio_%u -->
-	//     app | gstreamer
-	lastID int
-	stream *Broadcast
-	pipe   *gst.Pipeline
-	source *gst.Element
-	demux  *gst.Element
-	outs   []*splitter
-	stop   chan int
+	//     app | gstreamer pipeline
+	lastID       int
+	stream       *Broadcast
+	pipe         *gst.Pipeline
+	source       *gst.Element
+	demux        *gst.Element
+	demuxHandler glib.SigHandlerId
+	outs         []*splitter
+	stop         chan int
+	active       bool
 }
 
 type splitter struct {
 	// pad --> decode --> tee --+-- pad -->
 	//                          +-- pad -->
 	//                          ...
-	lastPadID   int
-	isVideo     bool
-	isVP9OrOpus bool // Exact meaning determined by `isVideo`.
-	demux       *Demuxer
-	source      *gst.Pad
-	decode      *gst.Element // May be nil, in which case source is copied as is.
-	tee         *gst.Element
-	fakesink    *gst.Element // Have to push the data somewhere when noone is reading.
+	lastPadID int
+	name      string
+	kind      string
+	demux     *Demuxer
+	source    *gst.Pad
+	decode    *gst.Element // May be nil, in which case source is copied as is.
+	tee       *gst.Element
+	fakesink  *gst.Element // Have to push the data somewhere when noone is reading.
 }
 
 type Muxer struct {
@@ -45,44 +44,53 @@ type Muxer struct {
 	//                           +--> muxer --> sink -|-> stream --+
 	// pad --> coder --> queue --/                    |            \--> client
 	//                                      gstreamer | app
-	demux  *Demuxer
-	inputs []*gst.Pad
-	coders []*gst.Element // May be nil if the corresponding splitter has nil decoder.
-	queues []*gst.Element
-	muxer  *gst.Element
-	sink   *gst.Element
+	demux       *Demuxer
+	inputs      []*gst.Pad
+	coders      []*gst.Element // May be nil if the corresponding splitter has nil decoder.
+	queues      []*gst.Element
+	muxer       *gst.Element
+	sink        *gst.Element
+	sinkHandler glib.SigHandlerId
 	Broadcast
 }
 
-func (d *Demuxer) obtainName(kind string) string {
-	d.lastID += 1
-	return fmt.Sprintf("%s_%d", kind, d.lastID)
-}
+var lastDemuxerID = 0
 
 func NewDemuxer(stream *Broadcast) (*Demuxer, error) {
 	d := Demuxer{stream: stream, stop: make(chan int)}
+	d.pipe = gst.NewPipeline(fmt.Sprintf("Transcoder %d", lastDemuxerID))
 
-	d.pipe = gst.NewPipeline(d.obtainName("pipeline"))
-
-	d.demux = gst.ElementFactoryMake("matroskademux", d.obtainName("demuxer"))
-	if d.demux == nil {
-		return nil, errors.New("could not create matroskademux")
+	var err error
+	if d.demux, err = d.createElement("matroskademux"); err != nil {
+		return nil, err
 	}
-	d.demux.ConnectNoi("pad-added", d.newOut, nil)
-
-	d.source = gst.ElementFactoryMake("appsrc", d.obtainName("appsrc"))
-	if d.source == nil {
-		return nil, errors.New("could not create appsrc")
+	if d.source, err = d.createElement("appsrc"); err != nil {
+		return nil, err
 	}
 	d.source.SetProperty("max-bytes", 8192)
-
-	d.pipe.Add(d.source, d.demux)
 	d.source.Link(d.demux)
+	return &d, nil
+}
+
+func (d *Demuxer) Run() {
+	d.pipe.SetState(gst.STATE_PLAYING)
+	d.active = true
+	defer d.pipe.SetState(gst.STATE_NULL)
+
+	d.demuxHandler = d.demux.ConnectNoi("pad-added", d.createSplitter, nil)
+	defer d.demux.Disconnect(d.demuxHandler)
+
+	ch := make(chan []byte, 30)
+	defer close(ch)
+
+	d.stream.Connect(ch, false)
+	defer d.stream.Disconnect(ch)
 
 	go func() {
 		bus := d.pipe.GetBus()
 		for {
 			msg := bus.TimedPopFiltered(6000000, gst.MESSAGE_ERROR)
+			// TODO stop sometime
 			if msg != nil {
 				err, _ := msg.ParseError()
 				fmt.Println(err.Error(), err.Code)
@@ -90,24 +98,6 @@ func NewDemuxer(stream *Broadcast) (*Demuxer, error) {
 			}
 		}
 	}()
-
-	go d.run()
-	return &d, nil
-}
-
-func (d *Demuxer) Close() {
-	d.stop <- 1
-}
-
-func (d *Demuxer) run() {
-	d.pipe.SetState(gst.STATE_PLAYING)
-	defer d.pipe.SetState(gst.STATE_NULL)
-
-	ch := make(chan []byte, 30)
-	defer close(ch)
-
-	d.stream.Connect(ch, false)
-	defer d.stream.Disconnect(ch)
 
 	for {
 		select {
@@ -124,67 +114,75 @@ func (d *Demuxer) run() {
 		}
 	}
 	d.source.Emit("end-of-stream")
+	d.active = false
 }
 
-func (d *Demuxer) newOut(pad *gst.Pad) {
+func (d *Demuxer) Close() {
+	d.stop <- 1
+}
+
+func (d *Demuxer) createElement(kind string) (*gst.Element, error) {
+	// All element within a single pipeline must have unique names.
+	// Problem is, we're generating muxers dynamically...
+	d.lastID += 1
+
+	if elem := gst.ElementFactoryMake(kind, fmt.Sprintf("%s_%d", kind, d.lastID)); elem != nil {
+		d.pipe.Add(elem)
+		if d.active {
+			elem.SetState(gst.STATE_PLAYING)
+		}
+		return elem, nil
+	}
+
+	return nil, errors.New(fmt.Sprintf("could not create %s", kind))
+}
+
+func (d *Demuxer) removeElement(e *gst.Element) {
+	e.SetState(gst.STATE_NULL)
+	d.pipe.Remove(e)
+}
+
+func (d *Demuxer) createSplitter(pad *gst.Pad) {
 	kind, _ := pad.GetCurrentCaps().GetStructure(0)
 
-	s := splitter{
-		isVideo:     strings.HasPrefix(kind, "video/"),
-		isVP9OrOpus: kind == "video/x-vp9" || kind == "audio/x-opus",
-		demux:       d,
-		source:      pad,
-	}
+	s := splitter{demux: d, name: pad.GetName(), kind: kind}
 
-	if s.isVideo {
-		if s.isVP9OrOpus {
-			s.decode = gst.ElementFactoryMake("vp9dec", d.obtainName("vp9dec"))
-		} else {
-			s.decode = gst.ElementFactoryMake("vp8dec", d.obtainName("vp8dec"))
+	var err error
+	if s.fakesink, err = d.createElement("fakesink"); err != nil {
+		panic("resource leak") // FIXME
+	}
+	if s.tee, err = d.createElement("tee"); err != nil {
+		panic("resource leak") // FIXME
+	}
+	s.tee.GetRequestPad(fmt.Sprintf("src_0")).Link(s.fakesink.GetStaticPad("sink"))
+
+	if strings.HasPrefix(kind, "video/") {
+		var err error
+		decodeKind := "vp8dec"
+		if kind == "video/x-vp9" {
+			decodeKind = "vp9dec"
 		}
-		if s.decode == nil {
-			// TODO something???
-			return
+		if s.decode, err = d.createElement(decodeKind); err != nil {
+			panic("resource leak") // FIXME
 		}
-	}
-
-	s.tee = gst.ElementFactoryMake("tee", d.obtainName("tee"))
-	if s.tee == nil {
-		// TODO there may be nothing to do though...
-		return
-	}
-
-	s.fakesink = gst.ElementFactoryMake("fakesink", d.obtainName("fakesink"))
-	if s.fakesink == nil {
-		// TODO ??????
-		return
-	}
-
-	d.outs = append(d.outs, &s)
-	d.pipe.Add(s.tee, s.fakesink)
-
-	if s.decode != nil {
-		d.pipe.Add(s.decode)
-		pad.Link(s.decode.GetStaticPad("sink"))
 		s.decode.Link(s.tee)
-		s.decode.SetState(gst.STATE_PLAYING)
+		pad.Link(s.decode.GetStaticPad("sink"))
 	} else {
 		pad.Link(s.tee.GetStaticPad("sink"))
 	}
 
-	s.tee.GetRequestPad(fmt.Sprintf("src_0")).Link(s.fakesink.GetStaticPad("sink"))
-	s.tee.SetState(gst.STATE_PLAYING)
-	s.fakesink.SetState(gst.STATE_PLAYING)
+	d.outs = append(d.outs, &s)
 }
 
-func (s *splitter) Acquire() *gst.Pad {
+func (s *splitter) createOutput() *gst.Pad {
 	s.lastPadID += 1
 	pad := s.tee.GetRequestPad(fmt.Sprintf("src_%d", s.lastPadID))
 	return pad
 }
 
-func (s *splitter) Release(pad *gst.Pad) {
-	// ...?
+func (s *splitter) removeOutput(pad *gst.Pad) {
+	pad.SetActive(false)
+	s.tee.RemovePad(pad)
 }
 
 func (d *Demuxer) NewMuxer(targetRate int) (*Muxer, error) {
@@ -193,95 +191,83 @@ func (d *Demuxer) NewMuxer(targetRate int) (*Muxer, error) {
 		inputs:    make([]*gst.Pad, 0, len(d.outs)),
 		coders:    make([]*gst.Element, 0, len(d.outs)),
 		queues:    make([]*gst.Element, 0, len(d.outs)),
-		muxer:     gst.ElementFactoryMake("webmmux", d.obtainName("webmmux")),
-		sink:      gst.ElementFactoryMake("appsink", d.obtainName("appsink")),
 		Broadcast: NewBroadcast(),
 	}
-	if m.muxer == nil {
-		return nil, errors.New("could not create webmmux")
+
+	var err error
+	if m.muxer, err = d.createElement("webmmux"); err != nil {
+		panic("resource leak") // DAMMIT
 	}
-	if m.sink == nil {
-		return nil, errors.New("could not create appsink")
+	if m.sink, err = d.createElement("appsink"); err != nil {
+		panic("resource leak") // FIXME
 	}
 
-	// FIXME if the following code fails, these elements remain in the pipe.
-	d.pipe.Add(m.muxer, m.sink)
 	m.muxer.SetProperty("streamable", true)
 	m.muxer.Link(m.sink)
-	m.sink.ConnectNoi("new-sample", m.retrieveBuffer, nil)
+
 	m.sink.SetProperty("emit-signals", true)
+	m.sinkHandler = m.sink.ConnectNoi("new-sample", m.fetchSample, nil)
 
 	for _, s := range d.outs {
-		input := s.Acquire()
-		queue := gst.ElementFactoryMake("queue", d.obtainName("queue"))
-		if queue == nil {
-			return nil, errors.New("could not create queue")
+		input := s.createOutput()
+		queue, err := d.createElement("queue")
+		if err != nil {
+			panic("resource leak") // FIXME
 		}
 
 		var coder *gst.Element
 		if s.decode != nil {
-			if !s.isVideo {
-				return nil, errors.New("don't want to recode audio")
+			if !strings.HasPrefix(s.kind, "video/") {
+				panic(fmt.Sprintf("unexpected caps %s", s.kind))
 			}
-			if s.isVP9OrOpus {
-				coder = gst.ElementFactoryMake("vp9enc", d.obtainName("vp9enc"))
-			} else {
-				coder = gst.ElementFactoryMake("vp8enc", d.obtainName("vp8enc"))
+
+			coderKind := "vp8enc"
+			if s.kind == "video/x-vp9" {
+				coderKind = "vp9enc"
 			}
-			if coder == nil {
-				return nil, errors.New("could not create vp[89]dec")
+			if coder, err = d.createElement(coderKind); err != nil {
+				panic("resource leak") // FIXME
 			}
-			coder.SetProperty("keyframe-max-dist", 60)
+
 			coder.SetProperty("deadline", 1)
 			coder.SetProperty("end-usage", 1) // constant bitrate
 			coder.SetProperty("target-bitrate", targetRate*8)
-			// FIXME elements will remain in pipe if the next iteration fails.
-			d.pipe.Add(queue, coder)
+			coder.SetProperty("keyframe-max-dist", 60)
 			coder.Link(queue)
 			input.Link(coder.GetStaticPad("sink"))
 		} else {
-			d.pipe.Add(queue)
 			input.Link(queue.GetStaticPad("sink"))
 		}
 
-		queue.GetStaticPad("src").Link(m.muxer.GetRequestPad(s.source.GetName()))
+		queue.GetStaticPad("src").Link(m.muxer.GetRequestPad(s.name))
 		m.inputs = append(m.inputs, input)
 		m.coders = append(m.coders, coder)
 		m.queues = append(m.queues, queue)
 	}
 
-	for _, e := range m.coders {
-		if e != nil {
-			e.SetState(gst.STATE_PLAYING)
-		}
-	}
-	for _, e := range m.queues {
-		e.SetState(gst.STATE_PLAYING)
-	}
-	m.muxer.SetState(gst.STATE_PLAYING)
-	m.sink.SetState(gst.STATE_PLAYING)
 	return &m, nil
 }
 
-func (m *Muxer) retrieveBuffer() {
+func (m *Muxer) fetchSample() {
 	sample := gst.SampleFromPointer(m.sink.Emit("pull-sample"))
 	defer sample.Unref()
-
 	buffer := sample.GetBuffer()
-	info := buffer.Map(gst.MAP_READ)
-	if info == nil {
-		// ...
-		return
-	}
-	defer buffer.Unmap(info)
-
-	if _, err := m.Write(info.Data()); err != nil {
-		// ...
-		return
+	if info := buffer.Map(gst.MAP_READ); info != nil {
+		m.Write(info.Data())
+		buffer.Unmap(info)
 	}
 }
 
 func (m *Muxer) Close() {
-	// TODO unlink the elements
+	for i, s := range m.demux.outs {
+		s.removeOutput(m.inputs[i])
+		if m.coders[i] != nil {
+			m.demux.removeElement(m.coders[i])
+		}
+		m.demux.removeElement(m.queues[i])
+	}
+	m.sink.Disconnect(m.sinkHandler)
+	m.demux.removeElement(m.muxer)
+	m.demux.removeElement(m.sink)
 	m.Broadcast.Close()
 }
