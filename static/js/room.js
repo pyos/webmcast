8086831ecd1fd0c0090194aa4efcc766ad1a8bef
pyos <pyos100500@gmail.com>
2016-03-24 20:04:51 +0300

    Slightly improve client-side code.
    As always, I'm skeptical of the quality of these
    "improvements" of mine.

--- static/js/room.js
+++ static/js/room.js
@@ -1,46 +1,57 @@
 'use strict';
 
 
-let RPC = {
-    WebSocket: function (socket) {
-        let cbs_by_id   = {};
-        let cbs_by_code = {};
-        let id = 0;
+let RPC = function(url, ...objects) {
+    let cbs_by_id   = {};
+    let cbs_by_code = {};
+    let id = 0;
 
-        socket.onmessage = (ev) => {
-            let msg = JSON.parse(ev.data);
-
-            if (msg.id === undefined) {
-                if (msg.method in cbs_by_code)
-                    cbs_by_code[msg.method](...msg.params);
-            }
-
-            if (msg.id in cbs_by_id) {
-                let cb = cbs_by_id[msg.id];
-                delete cbs_by_id[msg.id];
-                if (msg.error === undefined)
-                    cb.resolve(msg.result);
-                else
-                    cb.reject(msg.error.message, msg.error.code, msg.error.data);
-            }
-        };
-
-        let send = (method, ...params) =>
-            new Promise((resolve, reject) => {
+    let socket = new WebSocket(url);
+    let self = {
+        send: (method, ...params) => {
+            return new Promise((resolve, reject) => {
                 socket.send(JSON.stringify({ jsonrpc: '2.0', id, method, params }));
                 cbs_by_id[id] = { resolve, reject };
                 id = (id + 1) & 0x7FFF;
             });
+        },
 
-        let callback = (code, cb) => {
+        callback: (code, cb) => {
             if (cb === undefined)
                 delete cbs_by_code[code];
             else
                 cbs_by_code[code] = cb;
-        };
+        },
+    };
 
-        return { send, callback };
-    },
+    socket.onopen = () => {
+        for (let object of objects)
+            object.onLoad(self);
+    };
+
+    socket.onclose = (ev) => {
+        for (let object of objects)
+            object.onUnload();
+    };
+
+    socket.onmessage = (ev) => {
+        let msg = JSON.parse(ev.data);
+
+        if (msg.id === undefined)
+            if (msg.method in cbs_by_code)
+                cbs_by_code[msg.method](...msg.params);
+
+        if (msg.id in cbs_by_id) {
+            let cb = cbs_by_id[msg.id];
+            delete cbs_by_id[msg.id];
+            if (msg.error === undefined)
+                cb.resolve(msg.result);
+            else
+                cb.reject(msg.error);
+        }
+    };
+
+    return self;
 };
 
 
@@ -59,42 +70,27 @@ let ViewNode = function (root, stream) {
         root.querySelector('.pad').remove();
     });
 
-    view.addEventListener('error', () => {
+    let onDone = () => {
         root.classList.remove('w-icon-loading');
         root.classList.add('uk-icon-warning');
-    });
-
-    view.addEventListener('ended', () => {
-        root.classList.remove('w-icon-loading');
-        root.classList.add('uk-icon-warning');
-    });
-
-    let onLoad = (socket) => {
-        rpc = socket;
-        // TODO measure connection speed, request a stream
-        view.src = `/stream/${stream}`;
-        view.play();
     };
 
-    let onUnload = () => {
-        rpc = null;
+    view.addEventListener('error', onDone);
+    view.addEventListener('ended', onDone);
+
+    return {
+        onLoad: (socket) => {
+            rpc = socket;
+            // TODO measure connection speed, request a stream
+            view.src = `/stream/${stream}`;
+            view.play();
+        },
+
+        onUnload: () => {
+            rpc = null;
+            onDone();
+        },
     };
-
-    let measure = (size) => {
-        if (!rpc)
-            return new Promise((resolve, _) => resolve(Infinity));
-
-        const start = window.performance.now();
-        console.log(start);
-
-        return rpc.send('.get_zeros', size).then(() => {
-            const end = window.performance.now();
-            console.log(end);
-            return (end - start) / 1000;
-        });
-    };
-
-    return { onLoad, onUnload, measure };
 };
 
 
@@ -103,24 +99,6 @@ let ChatNode = function (root) {
     let msg = log.querySelector('.message');
     let rpc = null;
     msg.remove();
-
-    let onLoad = (socket) => {
-        rpc = socket;
-        rpc.callback('Chat.Message', (name, text) => {
-            let entry = msg.cloneNode(true);
-            entry.querySelector('.name').textContent = name;
-            entry.querySelector('.text').textContent = text;
-            log.appendChild(entry);
-        });
-
-        rpc.send('Chat.RequestHistory');
-        root.classList.add('active');
-    };
-
-    let onUnload = () => {
-        rpc = null;
-        root.classList.remove('active');
-    };
 
     let form = root.querySelector('.input-form');
     let text = form.querySelector('.input');
@@ -154,25 +132,31 @@ let ChatNode = function (root) {
         }
     });
 
-    return { onLoad, onUnload };
+    return {
+        onLoad: (socket) => {
+            rpc = socket;
+            rpc.callback('Chat.Message', (name, text) => {
+                let entry = msg.cloneNode(true);
+                entry.querySelector('.name').textContent = name;
+                entry.querySelector('.text').textContent = text;
+                log.appendChild(entry);
+            });
+
+            rpc.send('Chat.RequestHistory');
+            root.classList.add('active');
+        },
+
+        onUnload: () => {
+            rpc = null;
+            root.classList.remove('active');
+        },
+    };
 };
 
 
 let stream = document.body.getAttribute('data-stream-id');
-let view = new ViewNode(document.querySelector('.w-view-container'), stream);
-let chat = new ChatNode(document.querySelector('.w-chat-container'));
-let socket = new WebSocket(`ws${window.location.protocol == 'https:' ? 's' : ''}://`
-                           + `${window.location.host}/stream/${stream}`);
-let rpc = new RPC.WebSocket(socket);
-
-socket.onopen = () => {
-    chat.onLoad(rpc);
-    view.onLoad(rpc);
-};
-
-socket.onclose = (ev) => {
-    chat.onUnload();
-    view.onUnload();
-};
-
-// TODO socket.onerror?
+let view   = new ViewNode(document.querySelector('.w-view-container'), stream);
+let chat   = new ChatNode(document.querySelector('.w-chat-container'));
+let rpc    = new RPC(`ws${window.location.protocol == 'https:' ? 's' : ''}://`
+                     + `${window.location.host}/stream/${stream}`,
+                     chat, view);
